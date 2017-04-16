6434a97c66741d57e01e5558aec1cfaa5f12778c
Sasan <s.faizollahzadeh@saedubai.com>
2017-04-16 22:17:09 +0400

    Added Prefab for character

---
+++ CIU 211 Pres-Project/Assets/Scripts/Camera.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera : MonoBehaviour
+{
+    public float xDist;
+    public float yDist;
+    public float zDist;
+    private Vector3 CamPos;
+    private GameObject CameraMain;
+    public float cameraYLookDisp, cameraXLookDisp;
+    private Vector3 cameraLookAt;
+
+    void Start()
+    {
+        CameraMain = GameObject.FindGameObjectWithTag("MainCamera");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CamPos = new Vector3(xDist + gameObject.transform.position.x, yDist + gameObject.transform.position.y, zDist + gameObject.transform.position.z);
+        cameraLookAt = new Vector3(cameraXLookDisp, cameraYLookDisp, 0) + gameObject.transform.position;
+        CameraMain.transform.position = CamPos;
+        CameraMain.transform.LookAt(cameraLookAt);
+    }
+}
--- CIU 211 Pres-Project/Assets/Scripts/Controller.cs
+++ CIU 211 Pres-Project/Assets/Scripts/Controller.cs
@@ -6,32 +6,39 @@ public class Controller : MonoBehaviour {
 
     private Rigidbody rb;
     private Animator anim;
-    private float x,Force,MaxSpeed;
+    public float x,Force,MaxSpeed;
 	void Start () {
         rb = gameObject.GetComponent<Rigidbody>();
-        anim = gameObject.GetComponent<Animator>();		
+        anim = gameObject.GetComponent<Animator>();
+        x = anim.GetFloat("xPos");
 	}
 
     private void Update()
     {
         rb.velocity = Vector3.ClampMagnitude(rb.velocity, MaxSpeed);
-        transform.LookAt(transform.position, rb.velocity);
+        transform.LookAt(transform.position + rb.velocity);
+        //anim.SetFloat("xPos", x);
 
     }
     // Update is called once per frame
     void FixedUpdate () {
         float x = Input.GetAxis("Horizontal");
+        anim.SetFloat("xPos", x);
         if(x < 0)
         {
-            rb.AddForce(Vector3.right * Force);
+            rb.AddForce(Vector3.left * Force);
+            anim.SetBool("isWalking", true);
+
         }
         if(x > 0)
         {
-            rb.AddForce(Vector3.left * Force);
+            rb.AddForce(Vector3.right * Force);
+            anim.SetBool("isWalking", true);
         }
         if ( x == 0)
         {
             rb.velocity = Vector3.zero;
+            anim.SetBool("isWalking", false);
         }
 	}
 }
