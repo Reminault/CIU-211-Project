bbdb9e663acdc004f13e01a82534f4804d60efca
Reminault <nilemc12@hotmail.com>
2017-04-20 11:39:20 +0400

    Audio additions

---
+++ CIU 211 Pres-Project/Assets/Scripts/Dialogue.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class Dialogue : MonoBehaviour {
+
+	public GameObject[] text;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void clicked()
+	{
+		foreach (GameObject hello in text) 
+		{
+			hello.SetActive (false);
+		}
+	}
+}
