c8373902c99c35a27667c44a574725e2b4c281ea
Reminault <nilemc12@hotmail.com>
2017-04-20 12:14:17 +0400

    Added Interactive Text

---
+++ CIU 211 Pres-Project/Assets/Scripts/TriggerDialogue.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerDialogue : MonoBehaviour {
+
+	public GameObject initialDialogue;
+	public GameObject Yes1;
+	public GameObject No1;
+
+	bool activeTrigger;
+
+	// Use this for initialization
+	void Start () 
+	{
+		activeTrigger = true;
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		if (activeTrigger == true) {
+			
+			if (other.tag == "Player") {
+				initialDialogue.SetActive (true);
+				Yes1.SetActive (true);
+				No1.SetActive (true);
+				activeTrigger = false;
+			}
+		}
+	}
+}
